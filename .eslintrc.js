16ee0eb0fb411c1712a0853d50abc47ebdd3fa6c
hgg <2130374@qq.com>
2024-03-24 14:31:09 +0800

    添加自定义侧边栏-更新日志

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   'rules': {
     'indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': 0,
     'quotes': ['error', 'single'],
     'semi': ['error', 'never'],
     'no-undef': ['off'],
